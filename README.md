bdd7f4f835c786df0b06b3f0de7960d022fe6bc8
TangyOrigami <csaenz613@gmail.com>
2024-03-02 13:25:33 -0600

    tackling kickstart

---
+++ README.md
@@ -0,0 +1,4 @@
+# .dotfiles
+
+- March 2, 2024:
+	Tried fixing lazy.nvim from kickstart.nvim; still not working.
--- nvim/init.lua
+++ nvim/init.lua
@@ -204,6 +204,11 @@ local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
