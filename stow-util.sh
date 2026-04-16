58abb6534ddda1b854dd874024174ab271ce652d
Carlos <csaenz613@gmail.com>
2026-04-15 20:36:02 -0500

    kanagawa theme is the theme baby

--- stow-util.sh
+++ stow-util.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+blacklist=(
+    ".git"
+    ".gitsubmodules"
+    ".gitignore"
+    "bash"
+    "tmux"
+    "wsl-term"
+)
+
 cd ~/.dotfiles || exit
-
-sourceable=("bash" "tmux")
-
 packages=$(ls -d */)
 
 for package in $packages; do
     package_name="${package%/}"
+
+    # Check if package is blacklisted
+    skip=false
+    for ignored in "${blacklist[@]}"; do
+        if [[ "$package_name" == "$ignored" ]]; then
+            skip=true
+            break
+        fi
+    done
+
+    if $skip; then
+        echo "Skipped:  $package_name"
+        continue
+    fi
+
     stow "$package_name"
-    echo "Stowed: $package_name"
+    echo "Stowed:   $package_name"
 done
